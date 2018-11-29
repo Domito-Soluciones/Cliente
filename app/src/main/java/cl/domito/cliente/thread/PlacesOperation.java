85858699ea64add82fa163ce36bff460906b8c00
j0s3-s4nch3z <frogapp2019@gmail.com>
2018-11-29 01:58:05 -0300

    primer ingreso places

--- app/src/main/java/cl/domito/cliente/thread/PlacesOperation.java
+++ app/src/main/java/cl/domito/cliente/thread/PlacesOperation.java
@@ -1,6 +1,8 @@
 package cl.domito.cliente.thread;
 
 import android.os.AsyncTask;
+import android.support.constraint.ConstraintLayout;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.gson.JsonObject;
@@ -10,15 +12,23 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 
 import cl.domito.cliente.R;
 import cl.domito.cliente.activity.MapsActivity;
 import cl.domito.cliente.activity.utils.ActivityUtils;
+import cl.domito.cliente.dominio.Usuario;
 
 public class PlacesOperation extends AsyncTask<String, Void, Void> {
 
     private WeakReference<MapsActivity> context;
-    private TextView textViewError;
+    ConstraintLayout constraintLayout;
+    TextView textView1;
+    TextView textView2;
+    TextView textView3;
+    TextView textView4;
+    TextView textView5;
+    TextView textView6;
 
     public PlacesOperation(MapsActivity activity) {
         context = new WeakReference<MapsActivity>(activity);
@@ -26,16 +36,54 @@ public class PlacesOperation extends AsyncTask<String, Void, Void> {
 
     @Override
     protected Void doInBackground(String... strings) {
-        JSONArray places = ActivityUtils.getPlaces(context.get(),strings[0],strings[1]);
-        context.get().runOnUiThread(ActivityUtils.mensajeError(context.get()));
-        for(int i = 0 ; i < places.length() ; i++)
-        {
-            try {
-                JsonObject object =(JsonObject) places.get(i);
-            } catch (JSONException e) {
-                e.printStackTrace();
+        String latitud = Usuario.getInstance().getLatitud() + "";
+        String longitud = Usuario.getInstance().getLongitud() + "";
+        MapsActivity mapsActivity = context.get();
+        constraintLayout = mapsActivity.findViewById(R.id.constrainLayoutPlaces);
+        textView1 = mapsActivity.findViewById(R.id.textViewRes1);
+        textView2 = mapsActivity.findViewById(R.id.textViewRes2);
+        textView3 = mapsActivity.findViewById(R.id.textViewRes3);
+        textView4 = mapsActivity.findViewById(R.id.textViewRes4);
+        textView5 = mapsActivity.findViewById(R.id.textViewRes5);
+        textView6 = mapsActivity.findViewById(R.id.textViewRes6);
+        JSONArray jsonArray = ActivityUtils.getPlaces(mapsActivity,latitud,longitud,strings[0]);
+        if(jsonArray != null) {
+            for (int i = 0; i < jsonArray.length(); i++) {
+                if(i < 6) {
+                    try {
+                        JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+                        String place = jsonObject.getString("description");
+                        switch (i) {
+                            case 0:
+                                textView1.setVisibility(View.VISIBLE);
+                                textView1.setText(place);
+                                break;
+                            case 1:
+                                textView2.setVisibility(View.VISIBLE);
+                                textView2.setText(place);
+                                break;
+                            case 2:
+                                textView3.setVisibility(View.VISIBLE);
+                                textView3.setText(place);
+                                break;
+                            case 3:
+                                textView4.setVisibility(View.VISIBLE);
+                                textView4.setText(place);
+                                break;
+                            case 4:
+                                textView5.setVisibility(View.VISIBLE);
+                                textView5.setText(place);
+                                break;
+                            case 5:
+                                textView6.setVisibility(View.VISIBLE);
+                                textView6.setText(place);
+                                break;
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-
         }
         return null;
     }
